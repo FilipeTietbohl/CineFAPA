d48a28338d043b40ac7c96f501e5f1cecfded881
Filipe Barros <filipedebarros@hotmail.com>
2023-11-29 17:58:23 -0300

    Tela pagamento quase ok

--- app/src/main/java/com/example/cinefapa/TelaLugares.kt
+++ app/src/main/java/com/example/cinefapa/TelaLugares.kt
@@ -1,11 +1,22 @@
 package com.example.cinefapa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TelaLugares : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_lugares)
+
+        val botaoContinuarTelaLugares = findViewById<Button>(R.id.tela_lugares_continuar)
+
+
+        botaoContinuarTelaLugares.setOnClickListener {
+            val irParaTelaPagamento = Intent(this, TelaPagamento::class.java)
+            startActivity(irParaTelaPagamento)
+        }
+
     }
 }
