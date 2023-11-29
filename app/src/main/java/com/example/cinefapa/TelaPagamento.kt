1653a1368d91e8ed58a015e18cd2b072b262b490
Filipe Barros <filipedebarros@hotmail.com>
2023-11-29 17:23:17 -0300

    Tela Ingresso OK

---
+++ app/src/main/java/com/example/cinefapa/TelaPagamento.kt
@@ -0,0 +1,11 @@
+package com.example.cinefapa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaPagamento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_pagamento)
+    }
+}
--- app/src/main/java/com/example/cinefapa/telaIngresso.kt
+++ app/src/main/java/com/example/cinefapa/telaIngresso.kt
@@ -2,10 +2,19 @@ package com.example.cinefapa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
 
 class telaIngresso : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_ingresso)
+
+        val botaoContinuarTelaIngresso = findViewById<Button>(R.id.tela_lugares_continuar)
+
+        botaoContinuarTelaIngresso.setOnClickListener {
+            val irParaLugares = Intent(this, TelaLugares::class.java)
+            startActivity(irParaLugares)
+        }
     }
 }
