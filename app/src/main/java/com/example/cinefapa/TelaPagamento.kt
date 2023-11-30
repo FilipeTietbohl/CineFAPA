6c276b7ab329c5f31c66c54713d9e08e3b62790e
Filipe Barros <filipedebarros@hotmail.com>
2023-11-30 11:07:41 -0300

    Visualmente app OK

--- app/src/main/java/com/example/cinefapa/TelaPagamento.kt
+++ app/src/main/java/com/example/cinefapa/TelaPagamento.kt
@@ -1,11 +1,21 @@
 package com.example.cinefapa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TelaPagamento : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_pagamento)
+
+        val botaoContinuarTelaResumo = findViewById<Button>(R.id.btn_pagar)
+
+
+        botaoContinuarTelaResumo.setOnClickListener {
+            val irParaTelaResumo = Intent(this, TelaResumo::class.java)
+            startActivity(irParaTelaResumo)
+        }
     }
 }
---
+++ app/src/main/java/com/example/cinefapa/TelaResumo.kt
@@ -0,0 +1,13 @@
+package com.example.cinefapa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaResumo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_resumo)
+
+
+    }
+}
